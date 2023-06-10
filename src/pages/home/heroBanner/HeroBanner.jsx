25707947403d5e6f087e897d9158dea7635a28ed
vishu100zoro <vishu100zoro@gmail.com>
2023-06-10 13:13:03 +0530

    error

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
 import "./style.scss";
 import useFetch from "../../../hooks/useFetch";
-import Img from "../../../components/lazyLoadingImage/img";
+import Img from "../../../components/lazyLoadingImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 const HeroBanner=()=>{
     const[background,setBackground]=useState("");
--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import SwitchTabs from "../../../components/switchtabs/Switchtabs";
+import SwitchTabs from "../../../components/switchtabs/Switchtabs.jsx";
 import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 const Popular=()=>{
